0a0c700aca11509372c6a981bf089ff2029bed8a
HITENDRAPAL3 <hitendrasinghpal3@gmail.com>
2022-06-18 20:43:23 +0530

    Enter

--- src/styles/GlobalComponents/index.js
+++ src/styles/GlobalComponents/index.js
@@ -30,7 +30,7 @@ export const SectionTitle = styled.h2`
   line-height: ${(props) => props.main ? '72px' : '56px'};
   width: max-content;
   max-width: 100%;
-  background: linear-gradient(121.57deg, #FFFFFF 18.77%, rgba(255, 255, 255, 0.66) 60.15%);
+  background: white;
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
   margin-bottom: 16px;
